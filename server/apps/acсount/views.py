81a00b1d3e036ef8dae5fa7d0c250a49643084a2
HerdaSinama <ya.zabil@inbox.ru>
2024-10-07 17:28:49 +0300

    Add new views according to task
    - UserListView: return list of users, then returns all users whose username starts with the '...'
    - UserDetailView: depending on the request GET or PUT user information
    - AnotherUserView:
    return the specific user data

--- server/apps/acсount/views.py
+++ server/apps/acсount/views.py
@@ -1,10 +1,11 @@
 from .models import User, Team
-from .serializers import UserSerializer, TeamSerializer
+from .serializers import (
+    UserSerializer,
+    UserDetailSerializer,
+    UserUpdateSerializer
+    )
 
-from django.shortcuts import get_list_or_404
-
-from rest_framework import viewsets, generics
-from rest_framework.response import Response
+from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
 
 
@@ -13,4 +14,36 @@ class UserListView(generics.ListAPIView):
     permission_classes = (IsAuthenticated,)
 
     def get_queryset(self):
-        pass
+        queryset = User.objects.all()
+        username = self.request.query_params.get('username', None)
+        if username:
+            queryset = queryset.filter(username__startswith=username)
+        return queryset
+    
+
+class UserDetailView(generics.RetrieveAPIView):
+    permission_classes = (IsAuthenticated,)
+
+    def get_serializer_class(self):
+        if self.request.method in ['PUT', 'PATCH']:
+            return UserUpdateSerializer
+        return UserDetailSerializer
+
+    def get_object(self):
+        return self.request.user
+    
+
+class AnotherUserView(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserDetailSerializer
+    permission_classes = (IsAuthenticated,)
+
+
+class UserUpdateView(generics.UpdateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserUpdateSerializer
+    permission_classes = (IsAuthenticated,)
+
+    def get_object(self):
+        return self.request.user
+
