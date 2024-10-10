e717a03eceeb67919d4500bf7ad3374a1cef4bda
HerdaSinama <ya.zabil@inbox.ru>
2024-10-10 23:33:58 +0300

    Add view LoginAPIView

--- server/apps/a12n/views.py
+++ server/apps/a12n/views.py
@@ -3,39 +3,58 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework_simplejwt.tokens import RefreshToken
 
-from .serializers import UserSerializer
+from .serializers import UserRegisterSerializer, UserLoginSerializer
+from .models import UserSession
 
-# Create your views here.
-class RegistrationAPIView(APIView):
+from datetime import timezone
 
-    def post(self, request):
-        serializer = UserSerializer(data=request.data)
-        if serializer.is_valid():
-            user = serializer.save()
-            refresh = RefreshToken.for_user(user)
-            refresh.payload.update({
-                'user_id': user.id,
-                'username': user.username
-            })
-            return Response({
-                'refresh': str(refresh),
-                'access': str(refresh.access_token),
-            }, status=status.HTTP_201_CREATED)
+
+# class RegistrationAPIView(APIView):
+
+#     def post(self, request):
+#         serializer = UserRegisterSerializer(data=request.data)
+#         if serializer.is_valid():
+#             user = serializer.save()
+#             refresh = RefreshToken.for_user(user)
+#             refresh.payload.update({
+#                 'user_id': user.id,
+#                 'username': user.username
+#             })
+#             return Response({
+#                 'refresh': str(refresh),
+#                 'access': str(refresh.access_token),
+#             }, status=status.HTTP_201_CREATED)
         
 
 class LoginAPIView(APIView):
 
     def post(self, request):
-        data = request.data
-        username = data.get('username')
-        password = data.get('password')
-        if username is None:
-            return Response(
-                {'error': 'username is required',},
-                status=status.HTTP_401_UNAUTHORIZED
+        serializer = UserLoginSerializer(data=request.data)
+        if serializer.is_valid():
+            data = serializer.validated_data
+
+            response = Response(
+                {'access_token': data['access_token']},
+                status=status.HTTP_200_OK
             )
-        if password is None:
-            return Response(
-                { 'error': 'password is required',},
-                status=status.HTTP_401_UNAUTHORIZED
+            try:
+                session = UserSession.objects.get(
+                    refresh_token=data['refresh_token']
+                )
+                expires_at = session.expires_at
+                max_age = (expires_at - timezone.now()).total_seconds()
+            except UserSession.DoesNotExist:
+                return Response(status=status.HTTP_400_BAD_REQUEST,)
+
+            response.set_cookie(
+                'refresh_token',
+                data['refresh_token'],
+                httponly=True,
+                path= '/api/auth/',
+                max_age=max_age,
+            )
+            return response
+        return Response(
+            serializer.errors,
+            status=status.HTTP_400_BAD_REQUEST,
             )
